1f602668f8bf94bb937d1ebd5d3d846d3a40c300
Nate Dube <nate@ndube.com>
2019-08-12 21:30:06 -0700

    Improve wallpaper quality, speed

--- dot-wallpaper_generator/index.js
+++ dot-wallpaper_generator/index.js
@@ -3,31 +3,25 @@ const Trianglify = require("trianglify");
 const sharp = require("sharp");
 
 // Generate a pattern and then grab the PNG data uri.
-const pngURI = Trianglify({
+const triangles = Trianglify({
   width: 2560,
   height: 1440,
-  cell_size: Math.random() * 200 + 40,
+  cell_size: 75,
   color_space: "hsl",
   seed: Date.now(), // TODO: One per day?
-  variance: Math.random(),
+  variance: 1,
   x_colors: "random"
   //color_function: (x, y) => {
   //  return "hsl(" + Math.floor(Math.abs(x * y) * 360) + ",80%,30%)";
   //}
-}).png();
+}).svg();
 
-// Strip off the uri part of the data uri, leaving the data.
-const data = pngURI.substr(pngURI.indexOf("base64") + 7);
-
-// Decode the base64 encoded blob into a buffer.
-const buffer = new Buffer.from(data, "base64");
+const buffer = new Buffer.from(triangles.outerHTML);
 
 // Set up an image processing pipeline.
 const convertStream = sharp(buffer)
-  .blur(50)
+  .blur(50) // Takes a long time but looks pretty.
   .png();
 
 // Write the buffer to stdout.
 convertStream.pipe(process.stdout);
-
-// TODO: Faster to get triangles as a canvas and feed RGB to sharp?
