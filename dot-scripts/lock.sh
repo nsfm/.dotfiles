3c12db918fcde6d958068ad37baf4747d417ff6a
Nate Dube <nate@ndube.com>
2020-01-11 23:27:24 -0800

    Lock prior to suspend

--- dot-scripts/lock.sh
+++ dot-scripts/lock.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+
 # -selective-blur geometry
 # -adaptive-blur
 maim -q -m 5 -u | convert -taint -scale 10% -scale 1000% - png:/tmp/sc.png
@@ -14,7 +15,38 @@ i3lock_options=(
   -e -f -i /tmp/sc.png
 )
 
-# Borrowed from the manpages for xss-lock
+# Borrowed from the xss-lock docs
+get_brightness() {
+    if [[ -z $sysfs_path ]]; then
+        xbacklight -get
+    else
+        cat $sysfs_path
+    fi
+}
+
+set_brightness() {
+    if [[ -z $sysfs_path ]]; then
+        xbacklight -steps 1 -set $1
+    else
+        echo $1 > $sysfs_path
+    fi
+}
+
+fade_brightness() {
+    if [[ -z $sysfs_path ]]; then
+        xbacklight -time $fade_time -steps $fade_steps -set $1
+    elif [[ -z $fade_step_time ]]; then
+        set_brightness $1
+    else
+        local level
+        for level in $(eval echo {$(get_brightness)..$1}); do
+            set_brightness $level
+            sleep $fade_step_time
+        done
+    fi
+}
+
+# Also borrowed from the xss-lock docs
 if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
     kill_i3lock() {
         pkill -xu $EUID "$@" i3lock
