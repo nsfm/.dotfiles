c4adf17745dc8ae5276da466b998aa350703cbe8
Nate Dube <nate@ndube.com>
2026-03-04 15:37:23 -0800

    Update for new laptop

--- dot-scripts/macos.sh
+++ dot-scripts/macos.sh
@@ -1,5 +1,19 @@
-if [ ! -f "~/.config" ]; then ln -s ~/.dotfiles/dot-config ~/.config fi
-if [ ! -f "~/.zshrc" ]; then ln -s ~/.dotfiles/dot-zshrc ~/.zshrc fi
-if [ ! -f "~/.vimrc" ]; then ln -s ~/.dotfiles/dot-vimrc ~/.vimrc fi 
-if [ ! -f "~/.gitconfig" ]; then ln -s ~/.dotfiles/dot-gitconfig ~/.gitconfig fi 
-if [ ! -f "~/.scripts" ]; then ln -s ~/.dotfiles/dot-scripts ~/.scripts fi
+if [ ! -f "~/.config" ]; then
+  ln -s ~/.dotfiles/dot-config ~/.config
+fi
+
+if [ ! -f "~/.zshrc" ]; then
+  ln -s ~/.dotfiles/dot-zshrc ~/.zshrc
+fi
+
+if [ ! -f "~/.vimrc" ]; then
+  ln -s ~/.dotfiles/dot-vimrc ~/.vimrc
+fi 
+
+if [ ! -f "~/.gitconfig" ]; then
+  ln -s ~/.dotfiles/dot-gitconfig ~/.gitconfig
+fi 
+
+if [ ! -f "~/.scripts" ]; then 
+  ln -s ~/.dotfiles/dot-scripts ~/.scripts
+fi
